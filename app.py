559f5958ec286722edde376e367df3ad4df43f2d
baqar mustafa <baqarmustafa@baqars-MacBook-Air.local>
2026-01-24 16:26:05 +0530

    Render productivity session form using Flask template Updated the root route to render the index HTML template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from database import initialize_database
 
 app = Flask(__name__)
 
 initialize_database()
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return "FocusLens backend running"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
