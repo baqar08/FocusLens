6c23c190169beac66d3504c1a96ca5423fd8782a
baqar mustafa <baqarmustafa@baqars-MacBook-Air.local>
2026-01-25 22:44:38 +0530

    Add task-based productivity insights Extended productivity analysis to identify the most frequent task type and display it

--- utils/analysis.py
+++ utils/analysis.py
@@ -3,24 +3,24 @@ def analyze_sessions(sessions):
         return {}
 
     time_counts = {}
-    total_duration = 0
     energy_counts = {}
+    task_counts = {}
+    total_duration = 0
 
     for session in sessions:
         time_period = session[2]
         duration = int(session[3])
         energy = session[4]
+        task = session[5]
 
         time_counts[time_period] = time_counts.get(time_period, 0) + 1
         energy_counts[energy] = energy_counts.get(energy, 0) + 1
+        task_counts[task] = task_counts.get(task, 0) + 1
         total_duration += duration
 
-    most_productive_time = max(time_counts, key=time_counts.get)
-    most_common_energy = max(energy_counts, key=energy_counts.get)
-    average_duration = total_duration // len(sessions)
-
     return {
-        "most_productive_time": most_productive_time,
-        "most_common_energy": most_common_energy,
-        "average_duration": average_duration
+        "most_productive_time": max(time_counts, key=time_counts.get),
+        "most_common_energy": max(energy_counts, key=energy_counts.get),
+        "most_common_task": max(task_counts, key=task_counts.get),
+        "average_duration": total_duration // len(sessions)
     }
