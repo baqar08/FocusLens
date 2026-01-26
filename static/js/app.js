62f8736057141bb14dc100031cad39b3a6954cd6
baqar mustafa <baqarmustafa@baqars-MacBook-Air.local>
2026-01-26 09:19:40 +0530

    Improve chart rendering robustness Added safe checks in chart rendering logic to prevent errors when data is missing or incomplete

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,37 @@
 fetch("/chart-data")
     .then(response => response.json())
     .then(data => {
-        new Chart(document.getElementById("timeChart"), {
-            type: "bar",
-            data: {
-                labels: Object.keys(data.time_distribution),
-                datasets: [{ data: Object.values(data.time_distribution) }]
-            }
-        })
+        if (!data || Object.keys(data).length === 0) {
+            return
+        }
 
-        new Chart(document.getElementById("taskChart"), {
-            type: "pie",
-            data: {
-                labels: Object.keys(data.task_distribution),
-                datasets: [{ data: Object.values(data.task_distribution) }]
-            }
-        })
+        if (data.time_distribution) {
+            new Chart(document.getElementById("timeChart"), {
+                type: "bar",
+                data: {
+                    labels: Object.keys(data.time_distribution),
+                    datasets: [{ data: Object.values(data.time_distribution) }]
+                }
+            })
+        }
 
-        new Chart(document.getElementById("energyChart"), {
-            type: "line",
-            data: {
-                labels: data.energy_trend.map((_, i) => i + 1),
-                datasets: [{ data: data.energy_trend }]
-            }
-        })
+        if (data.task_distribution) {
+            new Chart(document.getElementById("taskChart"), {
+                type: "pie",
+                data: {
+                    labels: Object.keys(data.task_distribution),
+                    datasets: [{ data: Object.values(data.task_distribution) }]
+                }
+            })
+        }
+
+        if (data.energy_trend && data.energy_trend.length > 0) {
+            new Chart(document.getElementById("energyChart"), {
+                type: "line",
+                data: {
+                    labels: data.energy_trend.map((_, i) => i + 1),
+                    datasets: [{ data: data.energy_trend }]
+                }
+            })
+        }
     })
